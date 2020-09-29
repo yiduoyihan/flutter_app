d252043e9382ab1c85ccce7a074fcc71f8aeea93
Your Name <you@example.com>
2020-09-29 10:26:01 +0800

    add some code

--- lib/main.dart
+++ lib/main.dart
@@ -23,20 +23,20 @@ class MyApp extends StatelessWidget {
       theme: new ThemeData(
         primarySwatch: Colors.blue,
       ),
-      //注册路由表
       initialRoute: "/", //名为"/"的路由作为应用的home(首页)
+      //注册路由表
       routes: {
         "/": (context) => MyHomePage(), //首页路由
-        "new_counter_page": (context) => Counter(), //计数器页面
-        "self_manager_state": (context) => SelfManagerState(), //自管理状态页面
+        "new_counter_page____": (context) => Counter(), //计数器页面
+        "self_manager_state__": (context) => SelfManagerState(), //自管理状态页面
         "parent_manager_state": (context) => ParentManagerState(), //父管理状态页面
-        "blend_manager_state": (context) => BlendManagerState(), //混合管理状态页面
-        "text_font_style": (context) => TextFontStyle(), //文本字体样式
-        "button_style": (context) => ButtonStyle(), //按钮
-        "pic_and_icon": (context) => PicAndIcon(), //图片和icon
-        "switch_and_checkbox": (context) => CheckBox(), //单选和复选框
-        "edit_text": (context) => EditText(), //输入框
-        "progressbar": (context) => ProgressBar(), //进度条
+        "blend_manager_state_": (context) => BlendManagerState(), //混合管理状态页面
+        "text_font_style_____": (context) => TextFontStyle(), //文本字体样式
+        "button_style________": (context) => ButtonStyle(), //按钮
+        "picture_and_icon____": (context) => PicAndIcon(), //图片和icon
+        "switch_and_checkbox_": (context) => CheckBox(), //单选和复选框
+        "edit_text_input_____": (context) => EditText(), //输入框
+        "progressbar_________": (context) => ProgressBar(), //进度条
       },
     );
   }
@@ -68,54 +68,34 @@ class MyHomePage extends StatelessWidget {
       children: [
         RaisedButton(
             child: Text("计数器"),
-            onPressed: () {
-              startActivity(context, "new_counter_page");
-            }),
+            onPressed: () => startActivity(context, "new_counter_page____")),
         RaisedButton(
             child: Text("自管理状态"),
-            onPressed: () {
-              startActivity(context, "self_manager_state");
-            }),
+            onPressed: () => startActivity(context, "self_manager_state__")),
         RaisedButton(
             child: Text("父管理状态"),
-            onPressed: () {
-              startActivity(context, "parent_manager_state");
-            }),
+            onPressed: () => startActivity(context, "parent_manager_state")),
         RaisedButton(
             child: Text("混合管理状态"),
-            onPressed: () {
-              startActivity(context, "blend_manager_state");
-            }),
+            onPressed: () => startActivity(context, "blend_manager_state_")),
         RaisedButton(
             child: Text("文本字体样式"),
-            onPressed: () {
-              startActivity(context, "text_font_style");
-            }),
+            onPressed: () => startActivity(context, "text_font_style_____")),
         RaisedButton(
             child: Text("文本字体样式"),
-            onPressed: () {
-              startActivity(context, "button_style");
-            }),
+            onPressed: () => startActivity(context, "button_style________")),
         RaisedButton(
             child: Text("图片和icon"),
-            onPressed: () {
-              startActivity(context, "pic_and_icon");
-            }),
+            onPressed: () => startActivity(context, "picture_and_icon____")),
         RaisedButton(
             child: Text("单选和复选"),
-            onPressed: () {
-              startActivity(context, "switch_and_checkbox");
-            }),
+            onPressed: () => startActivity(context, "switch_and_checkbox_")),
         RaisedButton(
             child: Text("输入框和表单"),
-            onPressed: () {
-              startActivity(context, "edit_text");
-            }),
+            onPressed: () => startActivity(context, "edit_text_input_____")),
         RaisedButton(
             child: Text("进度指示器"),
-            onPressed: () {
-              startActivity(context, "progressbar");
-            }),
+            onPressed: () => startActivity(context, "progressbar_________")),
       ],
     );
   }
