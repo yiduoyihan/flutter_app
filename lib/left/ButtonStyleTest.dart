6f848156a39e72c4b310b98765e17d8165d35800
yiduoyihan <414497786@qq.com>
2020-10-14 20:32:18 +0800

    修改

--- lib/left/ButtonStyleTest.dart
+++ lib/left/ButtonStyleTest.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class ButtonStyle extends StatelessWidget {
+class ButtonStyleTest extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
